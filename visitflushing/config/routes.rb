25e2232ae39f0a203775375bfe5a0aeb3b22de12
EC2 Default User <ec2-user@ip-172-31-55-204.ec2.internal>
2021-05-23 18:45:01 +0000

    start

---
+++ visitflushing/config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  resources :hotels
+  resources :clients
+  
+  root 'clients#new'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
